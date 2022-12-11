2974b48ebe3ebdc9cc54e678da1a7e00b65d54b6
Nicechamp <nicechampz@gmail.com>
2022-12-11 11:10:42 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sd;
+
+	for (sd = 0; sd <= 9; sd++)
+	printf("%d", sd);
+	printf("\n");
+
+	return (0);
+}

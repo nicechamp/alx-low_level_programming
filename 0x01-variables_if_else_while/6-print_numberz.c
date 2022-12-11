24790e4f30dd136ea1a1c5ba170d588217fd1699
Nicechamp <nicechampz@gmail.com>
2022-12-11 11:37:21 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sd;
+
+	for (sd = '0'; sd <= '9'; sd++)
+	putchar(sd);
+	putchar('\n');
+
+	return (0);
+}

1e8a497c642072904f09ea99cd47b130cebb1019
Nicechamp <nicechampz@gmail.com>
2022-12-14 13:57:43 -0800

    7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Check Holberton
+ * @r: An integer input
+ * Description: This function prints the last digit of a number
+ * Return: last digit of number r
+ */
+int print_last_digit(int r)
+{
+	int l;
+
+	if (r < 0)
+	l = -1 * (r % 10);
+	else
+	l = r % 10;
+
+	_putchar((l % 10) + '0');
+	return (l % 10);
+}

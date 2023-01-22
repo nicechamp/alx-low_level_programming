aab6749014b0ceba54a3d126197de0e778324568
nicechamp <nicechampz@gmail.com>
2023-01-22 11:34:34 -0800

    first

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * 4-new_dog - create a new dog
+ * @name: new name
+ * @owner: new owner
+ *
+ * Return: nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
